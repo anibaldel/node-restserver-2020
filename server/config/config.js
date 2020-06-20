b8690fdf739ecc528e517907bb00893018adfbe7
anibaldel <anibaldel2669@gmail.com>
2020-06-20 18:12:30 -0400

    Variable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -15,10 +15,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 let urlDB;
 
-/*if (process.env.NODE_ENV === 'dev') {
+if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
-} else {*/
-urlDB = 'mongodb+srv://anibaldel:v2sUuZxwPvmA9RZA@cluster0-jli1c.mongodb.net/cafe';
-//}
+} else {
+    urlDB = process.env.MONGO_URI;
+}
 
 process.env.URLDB = urlDB;
