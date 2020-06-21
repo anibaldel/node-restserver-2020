d0433f3a6c6bfcd54ae9635df3bd558613516dbe
anibaldel <anibaldel2669@gmail.com>
2020-06-21 19:40:03 -0400

    Login normal seccion 10

--- server/config/config.js
+++ server/config/config.js
@@ -13,12 +13,23 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // Base de datos
 //====================
 
+//====================
+// Vencimiento del Token
+//====================
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+//====================
+// SEED de autenticacion
+//====================
+
+process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo';
+
 let urlDB;
 
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = process.env.MONGO_URI;
+    urlDB = 'mongodb+srv://anibaldel:v2sUuZxwPvmA9RZA@cluster0-jli1c.mongodb.net/cafe';
 }
 
 process.env.URLDB = urlDB;
